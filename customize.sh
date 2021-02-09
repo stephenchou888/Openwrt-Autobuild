16c7cd49a1cce84fc5a80efc23672687242a77e7
stephenchou888 <48269736+stephenchou888@users.noreply.github.com>
2021-02-09 11:31:33 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -9,3 +9,5 @@
 #sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
 # 获取luci-app-adguardhome
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/diy-packages/luci-app-adguardhome
+# 获取主题
+git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git package/diy-packages/luci-theme-infinityfreedom
