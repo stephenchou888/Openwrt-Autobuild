2388d7e709a2fcee17e89afc4ec947bdc94a0ce4
stephenchou888 <48269736+stephenchou888@users.noreply.github.com>
2021-02-09 17:20:46 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -8,10 +8,10 @@
 #1. Modify default IP
 #sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
 # 获取luci-app-adguardhome
-git clone https://github.com/rufengsuixing/luci-app-adguardhome package/diy-packages/luci-app-adguardhome
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 # 获取主题
-git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git package/diy-packages/luci-theme-infinityfreedom
-git clone https://github.com/Kinodi/luci-theme-atmaterial package/diy-packages/luci-theme-atmaterial
+git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git package/luci-theme-infinityfreedom
+git clone https://github.com/Kinodi/luci-theme-atmaterial package/luci-theme-atmaterial
 # 获取hello world和依赖
-git clone https://github.com/jerrykuku/lua-maxminddb package/diy-packages/helloworld/lua-maxminddb
-git clone https://github.com/jerrykuku/luci-app-vssr package/diy-packages/helloworld/luci-app-vssr
+git clone https://github.com/jerrykuku/lua-maxminddb package/helloworld/lua-maxminddb
+git clone https://github.com/jerrykuku/luci-app-vssr package/helloworld/luci-app-vssr
