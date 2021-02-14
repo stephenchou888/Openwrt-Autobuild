bd603bb07c09801aa7e2ee8522444d6d4a04824c
stephenchou888 <48269736+stephenchou888@users.noreply.github.com>
2021-02-14 09:07:35 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -7,3 +7,4 @@
 #=================================================
 #1. Modify default IP
 #sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
+export GOPROXY=https://goproxy.cn,direct
