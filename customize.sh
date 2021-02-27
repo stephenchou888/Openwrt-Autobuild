e6c774c995d8f6f8b6829ec8048977b70373c503
stephenchou888 <48269736+stephenchou888@users.noreply.github.com>
2021-02-27 09:05:18 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -7,3 +7,5 @@
 #=================================================
 #1. Modify default IP
 #sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
+#2. 修改内核版本
+#sed -i 's/KERNEL_PATCHVER:=4.14/KERNEL_PATCHVER:=4.19/g' target/linux/x86/Makefile
